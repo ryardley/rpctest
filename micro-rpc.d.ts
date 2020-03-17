7da5eccd194b3781bdf1d348fe4b0d13430edc62
Rudi Yardley <contact@rudiyardley.com>
2020-03-18 04:43:39 +0700

    Remove cruft

--- micro-rpc.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@bufferapp/micro-rpc" {
-  export const rpc = (a: any) => any;
-  export const method = (name: string, callback: (...a: any[]) => any) => any;
-}
-
-declare module "@bufferapp/micro-rpc-client" {
-  export default any;
-}
