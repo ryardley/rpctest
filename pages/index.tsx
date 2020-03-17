94259d335e74297ade0c8e955e20c757cbee64bf
Rudi Yardley <contact@rudiyardley.com>
2020-03-18 04:56:21 +0700

    Simplify code

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,10 @@ const client = new RPCClient(services, {
   url: "/api/gateway"
 });
 
-// Now let's do a Remote Procedure Call
-async function rpc() {
-  const result = await client.call("add", { a: 2, b: 2 });
-  return result;
-}
-
 export default () => (
   <button
     onClick={async () => {
-      const text = await rpc();
-      alert(text);
+      alert(await client.call("add", { a: 2, b: 2 }));
     }}
   >
     What is 2 + 2?
